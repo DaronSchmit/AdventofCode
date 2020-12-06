39cff59db65351b072065ee88eee3d7633a4191d
Daron <darons23@gmail.com>
2020-12-05 21:36:57 -0600

    added them all together and console.logged

--- day1/test.js
+++ day1/test.js
@@ -7,10 +7,12 @@ let fuelTotal = 0;
 try {
   const data = fs.readFileSync('day1input.txt', 'utf8')
   let massArray = data.split("\n");
-  let totalMass = 0;
+  let totalFuel = 0;
   let fuelArray = [];
   massArray.forEach(mass => fuelArray.push(fuelEquation(mass)));
   console.log(fuelArray);
+  fuelArray.forEach(fuel => totalFuel += fuel)
+  console.log(totalFuel);
   
   
 } catch (err) {
