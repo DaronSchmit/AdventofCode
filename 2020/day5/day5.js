f607b10dc06783794c8957d3f975a248e8386829
Daron Schmit <darons23@gmail.com>
2021-04-06 14:17:33 -0500

    added boarding pass object

--- 2020/day5/day5.js
+++ 2020/day5/day5.js
@@ -5,17 +5,27 @@ const read = require("../../readAndSplit.js");
 //Get data from input
 const rawInput = readAndSplit('input.txt');
 
-console.log(rawInput);
+// console.log(rawInput);
 //slice seat IDs between row and column
-let input = rawInput.map(seatID =>  {
-  return {"rowID":seatID.slice(0,7), "columnID": seatID.slice(7)};
+let input = rawInput.map(seatString =>  {
+  return {"rowID":seatString.slice(0,7), "columnID": seatString.slice(7)};
   });
 
-//change row IDs to Binary
+function BoardingPass(seatString){
+  this.rawInput = seatString;
+    //translate row to binary
+    this.row = parseInt(seatString.slice(0,7).replace(/B/g, "1").replace(/F/g, "0"), 2);
+    //translate column to binary
+    this.col = parseInt(seatString.slice(7).replace(/R/g, "1").replace(/L/g, "0"), 2);
+    //calculate seatID
+    this.seatID = (this.row*8)+this.col;
+}
 
-
-//change column ID to Binary
-
-//Do some math
+BoardingPass.prototype.displayInfo = function(){
+  return `row: ${this.row} col:${this.col} seatID: ${this.seatID}`;
+};
 
 //console log
+let pass = new BoardingPass("BFFFBBFRRR");
+
+console.log(pass.displayInfo());
