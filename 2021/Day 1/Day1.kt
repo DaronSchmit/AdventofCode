283650c39d7833cfc7ca379b9c829d23880079ac
Daron Schmit <darons23@gmail.com>
2021-12-01 10:57:17 -0600

    day 1

---
+++ 2021/Day 1/Day1.kt
@@ -0,0 +1,32 @@
+import java.io.File
+
+fun main() {
+    fun readFile(filename: String): List<String> = File(filename).readLines()
+    var lines = readFile("input.txt").map{ it.toInt() }
+
+    fun compare(a: Int, b: Int) = if (a > b) "larger" else "smaller"
+
+    fun part1() {
+        var numLarger: Int = 0
+        for (i in 1 until lines.size) {
+            if(compare(lines[i], lines[i-1]) == "larger") numLarger++
+        }
+        println(numLarger)
+    }
+    // part1()
+
+    fun part2() {
+        var numLarger: Int = 0
+        var current: Int
+        var last: Int = lines[2]+lines[1]+lines[0]
+
+        for (i in 3 until lines.size) {
+            current =  lines[i]+lines[i-1]+lines[i-2]
+            if(compare(current, last) == "larger") numLarger++
+            last = current
+        }
+        println(numLarger)
+    }
+    part2()
+
+}
